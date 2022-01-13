7c0c92d1fdcec2f74047659ed896c6bd470bac8f
namesaregreat <tcaron@nait.ca>
2022-01-12 23:55:18 -0700

    January 12th Class Example

---
+++ January 12, 2022 Examples/January 12, 2022 Examples/Cat.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace January_12__2022_Examples
+{
+    class Cat
+    {
+        private string _name = null!;
+        private int _age;
+        private string _colour = null!;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if(value.Trim().Length > 0)
+                {
+                    _name = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid name.");
+                }
+            }
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if(value >= 0)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    throw new Exception("Age must be 0 or more.");
+                }
+            }
+        }
+
+        public string Colour
+        {
+            get { return _colour; }
+            set 
+            {
+                if (value.Trim().Length > 0)
+                {
+                    _colour = value;
+                }
+                else
+                {
+                    throw new Exception("Invalid colour.");
+                }
+            }
+        }
+
+        public Cat(string name, int age, string colour)
+        {
+            Name = name;
+            Age = age;
+            Colour = colour;
+        }
+
+        public Cat()
+        {
+
+        }
+    }
+}
---
+++ January 12, 2022 Examples/January 12, 2022 Examples/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+using January_12__2022_Examples;
+
+string Whatever = "      sadasdsa      ";
+
+Console.WriteLine(Whatever);
+Console.WriteLine(Whatever.Length);
+Console.WriteLine(Whatever.Trim());
+Console.WriteLine(Whatever.Trim().Length);
+Console.WriteLine(Something());
+
+Cat nullCat = new Cat();
+Cat sillyCat = new Cat("Panda", 0, "Black and White");
+
+Console.Write("Enter the cat's name: ");
+nullCat.Name = Console.ReadLine();
+
+Console.WriteLine(nullCat.Name);
+Console.WriteLine(sillyCat.Name);
+
+static string Something()
+{
+    return "A new string";
+}

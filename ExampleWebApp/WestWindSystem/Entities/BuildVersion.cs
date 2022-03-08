f3a5ee201cdf88d1b24b88e794d4e4b964bd0a14
namesaregreat <tcaron@nait.ca>
2022-03-07 23:29:55 -0700

    Added BuildVersion and Regions Entities.

---
+++ ExampleWebApp/WestWindSystem/Entities/BuildVersion.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace WestWindSystem.Entities
+{
+    [Table("BuildVersion")]
+    public class BuildVersion
+    {
+        [Key]
+        public int Id { get; set; }
+        public int Major { get; set; }
+        public int Minor { get; set; }
+        public int Build { get; set; }
+        public DateTime ReleaseDate { get; set; }
+
+    }
+}
---
+++ ExampleWebApp/WestWindSystem/Entities/Region.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Addition Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace WestWindSystem.Entities
+{
+    [Table("Regions")]
+    public class Region
+    {
+        [Key]
+        public int RegionID { get; set; }
+        [Required(ErrorMessage = "Region Description is required")]
+        [StringLength(50, ErrorMessage = "Region Description is limited to 50 characters")]
+        public string RegionDescription { get; set; }
+    }
+}

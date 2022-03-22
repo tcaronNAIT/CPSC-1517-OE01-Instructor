7064a651264d7a11ec5df2f2005a7364f7877420
namesaregreat <tcaron@nait.ca>
2022-03-21 23:49:21 -0600

    Commit first part of pagination

--- ExampleWebApp/BobWebApp/Pages/Samples/PartialFilterSearch.cshtml.cs
+++ ExampleWebApp/BobWebApp/Pages/Samples/PartialFilterSearch.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 #region Additional Namespaces
 using WestWindSystem.BLL;
 using WestWindSystem.Entities;
+using WebApp.Helpers;
 #endregion
 
 namespace ExampleWebApp.Pages.Samples
@@ -29,11 +30,36 @@ namespace ExampleWebApp.Pages.Samples
 
         public List<Territory> TerritoryResults { get; set; } = new List<Territory>();
 
-        public void OnGet()
+        #region Paginator
+        //set the desired page size
+        private const int PAGE_SIZE = 5;
+        //be able to hold an instance of the Paginator Class
+        public Paginator Pager { get; set; }
+        #endregion
+
+        public void OnGet(int? currentPage)
         {
-                if(!string.IsNullOrEmpty(partialSearchText))
+
+            //using the Paginator with your query
+
+            //OnGet ends up with a parameter (Request query string) that receives the current
+            // page number. On the initial load of the page, this value will be null
+            // to be null we had to add the ? after int.
+            if(!string.IsNullOrEmpty(partialSearchText))
 			{
-                TerritoryResults = _territoryServices.GetByPartialDescription(partialSearchText);
+                //temp value for the number of results for our query
+                int totalCount;
+                //set up for using the paginator if a query is run, no need if no query
+
+                // determine the current page number
+
+                int pageNumber = currentPage.HasValue ? currentPage.Value : 1;
+                //set up the current state of the paginator (with the sizing)
+                PageState current = new(pageNumber, PAGE_SIZE);
+                //do our query
+                TerritoryResults = _territoryServices.GetByPartialDescription(partialSearchText, pageNumber, PAGE_SIZE, out totalCount);
+
+                Pager = new Paginator(totalCount, current);
 			}
         }
 
@@ -45,6 +71,13 @@ namespace ExampleWebApp.Pages.Samples
             }
 
             return RedirectToPage(new { partialSearchText = partialSearchText });
+        }
+
+        public IActionResult OnPostClear()
+        {
+            partialSearchFeedback = "";
+            ModelState.Clear();
+            return RedirectToPage(new { PartialSearchText = (string?)null });
         }
     }
 }
--- ExampleWebApp/WestWindSystem/BLL/TerritoryServices.cs
+++ ExampleWebApp/WestWindSystem/BLL/TerritoryServices.cs
@@ -26,11 +26,24 @@ namespace WestWindSystem.BLL
         #region Queries
 
         //query by a string
-        public List<Territory> GetByPartialDescription(string partialDescription)
+        public List<Territory> GetByPartialDescription(string partialDescription, int pageNumber, int pageSize, out int totalCount)
 		{
-            return _context.Territories
+            int skipRows;
+            IEnumerable<Territory> info = _context.Territories
                                     .Where(territory => territory.TerritoryDescription.Contains(partialDescription))
-                                    .OrderBy(territory => territory.TerritoryDescription).ToList();
+                                    .OrderBy(territory => territory.TerritoryDescription);
+            //set the totalCount so it can be provided to the caller of the method.
+            totalCount = info.Count();
+            //determine the number of rows to skip (if I'm on page 4 I need to not
+            //return the first 3 pages of records.
+            //(index - 1) * pageSize
+            //PageNumber needs to be treated as a index (natural number - 1)
+            skipRows = (pageNumber - 1) * pageSize;
+            //return only the required number of rows
+            //this will be done using filters through Linq
+            //use the filter .Skip(n) to skip over n rows from the beginning of the collection
+            //use the filter .Take(n) to take only the next n rows from the collection
+            return info.Skip(skipRows).Take(pageSize).ToList();
 		}
 
         #endregion
